cd2a5fba3424ef8ba331fe91f763e6da14ab4616
HamiInanGumus <98495231+HamiInanGumus@users.noreply.github.com>
2022-01-26 17:44:59 -0500

    Add files via upload

---
+++ rss_scraper_collocations.py
@@ -0,0 +1,19 @@
+# This program calculates the collocations in the tokenized entries/articles
+# of an rss/atom feed after the url of the feed is provided by the user.
+# It provides the output in numerical order of the entries/articles.
+# The uses rss_scraper_tokenizedtext_class and its method to scrape the feed
+# and instantiate tokenized text.
+
+import nltk, re, pprint
+from rss_scraper_tokenizedtext_class import *
+
+rss_tokenized_entries = RssTokenizedText.tokenize()
+collocations_list = []
+
+def collocations():
+    for article in rss_tokenized_entries:
+        print(rss_tokenized_entries.index(article)+1)
+        text = nltk.Text(article)
+        text1 = text.collocations()
+
+collocations()
